b45373bd33ee922ae483a0cdffe995117483eb31
Dmytro Nemoga <dmytro.nemoga@datagazer.io>
2019-01-14 15:39:19 +0200

    Fixed DS block diffuculty and gas price outputs

--- src/packages/core/src/services/zilliqa.service.ts
+++ src/packages/core/src/services/zilliqa.service.ts
@@ -42,6 +42,7 @@ export class ZilliqaService {
     return this.$httpClient.post<JsonRpcResponse<any>>('/zilliqa', request).pipe(
       map(({ result }) => ({
         ...result,
+        gasPrice: Number(result.gasPrice) * Math.pow(10, -12),
         fromAddr: this._getAddressFromPublicKey(result.senderPubKey)
       }))
     );
